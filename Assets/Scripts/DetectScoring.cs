f19f23d38b6ec61777615a00d1767557ca6218ef
david carlson <dcarl661@gmail.com>
2016-11-05 10:25:50 -0700

    add scorekeeper and target prefab

---
+++ Assets/Scripts/DetectScoring.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectScoring : MonoBehaviour {
+
+    public int scorePerHit = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //this DetectScoring object is on the Target
+    void OnCollisionEnter(Collision collision)
+    {
+        //FindObjectOfType unity finds objects not necessarily gameobjects
+        // but in the heirarchy 
+        ScoreKeeper scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        scoreKeeper.IncrementScore(scorePerHit);
+    }
+
+
+}
